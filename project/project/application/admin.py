fccb11b47c3dde29c93f63a442f56f3dfba89f80
sanch27 <sanchaliannavarapu@gmail.com>
2024-02-27 21:32:10 +0530

    updated changes

--- project/project/application/admin.py
+++ project/project/application/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Record
+from .models import Record, Courses, Packages, PackageOptions, Subscriptions
 
 admin.site.register(Record)
+admin.site.register(Courses)
+admin.site.register(Packages)
+admin.site.register(PackageOptions)
+admin.site.register(Subscriptions)
+
--- project/project/application/models.py
+++ project/project/application/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Record(models.Model):
@@ -14,3 +15,42 @@ class Record(models.Model):
 
 	def __str__(self):
 		return(f"{self.first_name} {self.last_name}")
+	
+
+
+
+class Courses(models.Model):
+    CourseID = models.AutoField(primary_key=True)
+    CourseName = models.CharField(max_length=255)
+    CourseDescription = models.TextField()
+
+    def __str__(self):
+        return(f"{self.CourseName}")
+
+class Packages(models.Model):
+    PackageID = models.AutoField(primary_key=True)
+    PackageName = models.CharField(max_length=255)
+    PackageDescription = models.TextField()
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return(f"{self.PackageName}")
+
+class PackageOptions(models.Model):
+    OptionID = models.AutoField(primary_key=True)
+    PackageID = models.ForeignKey(Packages, on_delete=models.CASCADE)
+    CourseID = models.ForeignKey(Courses, on_delete=models.CASCADE)
+    
+
+
+class Subscriptions(models.Model):
+    SubscriptionID = models.AutoField(primary_key=True)
+    UserID = models.ForeignKey(User, on_delete=models.CASCADE)
+    PackageID = models.ForeignKey(Packages, on_delete=models.CASCADE)
+    PaymentDate = models.DateField()
+    ExpiryDate = models.DateField()
+
+    def __str__(self):
+        return (f"SubscriptionID: {self.SubscriptionID}, UserID: {self.UserID}, PackageID: {self.PackageID}, PaymentDate: {self.PaymentDate}, ExpiryDate: {self.ExpiryDate}")
+
+
